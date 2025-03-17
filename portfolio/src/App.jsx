031017d38e4619059910f0a58d97e48efb5bd657
Minindu Bimsara Abeywardena <minindubim@gmail.com>
2025-03-18 00:34:25 +0530

    Add assets, update component imports, and configure routing

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,4 +1,8 @@
 // Desc: Main App component
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './Pages/Home/Home'
 import './App.css'
 
 function App() {
@@ -6,9 +10,17 @@ function App() {
 
   return (
     <>
-      
+      <Router>
+        <div>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="*" element={<div>404 Not Found</div>}></Route>
+          </Routes>
+        </div>
+      </Router>
     </>
-  )
+  );
 }
 
 export default App
