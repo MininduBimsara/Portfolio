06447963eff6abec10fbd34513830ce3ee0aa398
Minindu Bimsara Abeywardena <minindubim@gmail.com>
2025-05-21 00:04:38 +0530

    Remove commented-out Testimonial component from Home page

--- portfolio/src/Pages/Home/Home.jsx
+++ portfolio/src/Pages/Home/Home.jsx
@@ -51,9 +51,9 @@ export default function Home() {
             <MyPortfolio />
           </div>
 
-          <div className="">
+          {/* <div className="">
             <Testimonial />
-          </div>
+          </div> */}
 
           <div className="">
             <ContactMe />
